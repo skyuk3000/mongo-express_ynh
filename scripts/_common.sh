0b9fa59a65d3bca37eb3db93106e53c821504d2d
Gérard Collin <gerard.collin@appliweb.net>
2022-12-29 10:06:46 +0100

    Small corrections

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,6 +2,7 @@
 
 #=================================================
 # COMMON VARIABLES
+#=================================================
 nodejs_version=16
 
 # dependencies used by the app (must be on a single line)
